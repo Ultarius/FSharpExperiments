7088277bd216da7ac373f6cbb1d7ebdb9954fe0f
Damien Rietdijk <damien59881@gmail.com>
2016-03-09 16:07:23 +0100

    CoroutineTest

--- SFMLTransportGame/SFMLTransportGame/Entity.fs
+++ SFMLTransportGame/SFMLTransportGame/Entity.fs
@@ -3,14 +3,41 @@
 open SFML.Graphics
 open SFML.System
 
+type Coroutine<'w, 'so, 'si, 'a> = 'w -> 'so -> 'si -> Result<'w, 'so, 'si, 'a>
+and Result<'w, 'so, 'si, 'a> = 
+    | Done of 'so * 'si * 'a
+    | Busy of 'so * 'si * Coroutine<'w, 'so, 'si, 'a>
+
+let ret (x:'a) : Coroutine<'w, 'so, 'si, 'a> =
+    fun w so si -> Done(so,si,x)
+
+let rec (>>=) (p:Coroutine<'w, 'so, 'si, 'a>) (k:'a->Coroutine<'w, 'so, 'si, 'b>) : Coroutine<'w, 'so, 'si, 'b> =
+    fun w so si ->
+        match p w so si with
+        | Done (so', si', a)    -> k a w so' si'
+        | Busy (so', si', p')   -> Busy (so', si', p' >>= k)
+
+type CoroutineBuilder() =
+    member this.Return x = ret x
+    member this.Bind(p,k) = p >>= k
+
+let co = CoroutineBuilder()
+
+
+let co_step result =
+    match result with
+    | Done(so, si, res) -> so, si, co{return res}
+    | Busy(so, si, c') -> so, si, c'
 
 type Behaviour<'d> = Behaviour of ('d -> 'd * Behaviour<'d>)
 
-type Entity<'d> = 
+
+
+type Message =
     {
-        Data        : 'd
-        Behaviours  : Behaviour<'d> list 
-        Render      : 'd -> RectangleShape
+        To : int
+        From : int
+        Message : string
     }
 
 type FactoryData =
@@ -43,11 +70,31 @@ type EntityData =
     | Truck of TruckData
     | Road of RoadData
 
-let rec repeat c x =
+
+type Entity<'d> = 
+    {
+        Data        : 'd
+        Render      : 'd -> RectangleShape
+    }
+
+type GameState = 
+    {
+        Entities  : Entity<EntityData> list
+        Messages  : Message list
+        Routines  : Coroutine<GameState, Message list, Entity<EntityData> list, Unit> list
+    }
+
+let rec repeatgg c x =
     match c with
     | Behaviour(cfunc) ->   let x', c' = cfunc x 
-                            //printfn "%A" x'
                             x'
+
+
+let rec repeat c (x : EntityData) (m : Message list) =
+    match c with
+    | Done(so',si',x) ->   let x', c' = so' x m
+                           x' 
+    | Busy (so, si, p') -> si
 
 
 let rec AIBehaviour = Behaviour(fun (x : EntityData ) -> match x with
@@ -63,13 +110,68 @@ let rec Drawable = Behaviour(fun (x : EntityData ) -> match x with
                                                                 | Factory x ->  (x |> Factory, Drawable)
                                                                 | _ -> (x, Drawable))
 
+let TruckArrived (t : TruckData) (m : Message list) : (EntityData * Message list) =
+    if t.Arrived then 
+        let newTruck = {t with Arrived = false}
+        let message = {To = 0; From = 1; Message = "Arrived"}
+        (newTruck |> Truck, (@) [message] m)
+    else
+        (t |> Truck, m)
+    
+let TruckAI (t : TruckData) (m : Message list) : (EntityData * Message list) =
+    if t.Arrived && (t.Capacity < t.MaxCapacity) then 
+        let rec messageReader list =
+            match list with
+                | [] when t.Capacity < t.MaxCapacity -> ({t with Capacity = t.Capacity + 1} |> Truck, (@) [{To = 0; From = 1; Message = "Request"}] m)
+                | [] -> (t |> Truck, m)
+                | x::xs when x.Message = "Supply" -> ({t with Capacity = t.Capacity + 1} |> Truck, m)
+                | x::xs -> messageReader xs
+        messageReader m
+    elif t.Capacity >= t.MaxCapacity && t.Position.X > -75.0f then
+        ({t with Position = new Vector2f(t.Position.X - t.Speed, t.Position.Y)} |> Truck, m)
+    elif t.Capacity = 0 && t.FactoryData.Position.X > t.Position.X then
+        ({t with Position = new Vector2f(t.Position.X + t.Speed, t.Position.Y)} |> Truck, m)
+    elif t.Arrived = false && t.Position.X >= t.FactoryData.Position.X then
+        ({t with Arrived = true} |> Truck, m)
+    else
+        ({t with Capacity = 0; Arrived = false} |> Truck, m)
+    
+ 
+(*
+                                           
+let rec PickupEvent = Queue(fun (x : EntityData ) (messages : Message list) -> match x with
+                                                                                | Truck x -> (TruckArrived x messages), PickupEvent
+                                                                                | Factory x ->  ((x |> Factory), messages), PickupEvent
+                                                                                | _ -> (x, messages), PickupEvent)    
+                                           
+let rec AI = Queue(fun (x : EntityData ) (messages : Message list) -> match x with
+                                                                                | Truck x -> (TruckAI x messages), AI
+                                                                                | Factory x ->  ((x |> Factory), messages), AI
+                                                                                | _ -> (x, messages), AI)
+
+*)
+
+//let coAI : Coroutine<GameState, Message list,'d, Unit> = fun x b c -> match c b x with | Done(so', d', k') -> let x', c' = so' x b 
+
+
+let MainLoop : Coroutine<GameState, Message list, Entity<EntityData> list, Unit> =
+                                                         fun x b c ->   
+                                                                        //let newEntities, newMessages = c |> List.fold(fun (entities, messages) entity -> match entity.Data with | _ -> (entity::, b)) (c, b)
+                                                                        //let flod = c |> List.fold(fun (newMessage, newEntities, messages) entity -> List.fold (fun (newMessages, newEntities, entity) msg -> (newMessages, newEntities, entity)) (newMessages, newEntities, entity) messages) ([], [], b) c
+                                                                        let newE = c |> List.map(fun e -> match e.Data with
+                                                                                                                    | Factory x -> e
+                                                                                                                    | Truck x -> let truckData, messages = TruckAI x b
+                                                                                                                                 {e with Data = truckData }
+                                                                                                                    | _ -> e)
+                                                                        Done(b, newE, ())
+                                                                                             
+
 let addRoad amount (y : float32) : Entity<EntityData> list =
     [
         for i = 0 to amount do
           yield
             {
                 Data = {Position = new Vector2f(float32(i * 50), y);} |> Road;
-                Behaviours = [];
                 Render = fun (Road x) -> 
                     new RectangleShape(new Vector2f(50.0f, 25.0f), Position = new Vector2f(x.Position.X, x.Position.Y), FillColor=Color.Black)
             }
--- SFMLTransportGame/SFMLTransportGame/Game.fs
+++ SFMLTransportGame/SFMLTransportGame/Game.fs
@@ -6,32 +6,30 @@ open SFML.System
 
 open Entity
 
-type GameState = 
-    {
-        Entities  : Entity<EntityData> list
-    }
 
 type Game = {
     Init : RenderWindow -> unit
     Run : GameState -> RenderWindow -> unit
 }
 
+
+
 let rec TransportGame : Game = 
     {   
         Init = fun (window : RenderWindow ) ->
             let factoryData : FactoryData = {FactoryData.Position = new Vector2f(600.0f, 50.0f); RefreshRate = 10; Storage = 0; MaxStorage = 10;}
-            let factory = {Data = factoryData |> EntityData.Factory; Behaviours = []; Render = fun (Factory x) -> new RectangleShape(new Vector2f(95.0f, 80.0f), Position = new Vector2f(x.Position.X, x.Position.Y), FillColor=Color.Red)}
+            let factory = {Data = factoryData |> EntityData.Factory; Render = fun (Factory x) -> new RectangleShape(new Vector2f(95.0f, 80.0f), Position = new Vector2f(x.Position.X, x.Position.Y), FillColor=Color.Red)}
 
             let factoryData2 : FactoryData = {FactoryData.Position = new Vector2f(600.0f, 300.0f); RefreshRate = 10; Storage = 0; MaxStorage = 10;}
-            let factory2 = {Data = factoryData2 |> EntityData.Factory; Behaviours = []; Render = fun (Factory x) -> new RectangleShape(new Vector2f(95.0f, 80.0f), Position = new Vector2f(x.Position.X, x.Position.Y), FillColor=Color.Red)}
+            let factory2 = {Data = factoryData2 |> EntityData.Factory; Render = fun (Factory x) -> new RectangleShape(new Vector2f(95.0f, 80.0f), Position = new Vector2f(x.Position.X, x.Position.Y), FillColor=Color.Red)}
 
-            let truck = {Data = {Position = new Vector2f(0.0f, 107.0f); Speed = 2.5f; Capacity = 0; MaxCapacity = 300; Arrived = false; Loaded = false; FactoryData = factoryData;} |> EntityData.Truck; Behaviours = [PickupBehaviour; AIBehaviour]; Render = fun (Truck x) -> new RectangleShape(new Vector2f(40.0f, 20.0f), Position = new Vector2f(x.Position.X, x.Position.Y), FillColor=Color.White)}
+            let truck = {Data = {Position = new Vector2f(0.0f, 107.0f); Speed = 2.5f; Capacity = 0; MaxCapacity = 300; Arrived = false; Loaded = false; FactoryData = factoryData;} |> EntityData.Truck; Render = fun (Truck x) -> new RectangleShape(new Vector2f(40.0f, 20.0f), Position = new Vector2f(x.Position.X, x.Position.Y), FillColor=Color.White)}
         
-            let truck2 = {Data = {Position = new Vector2f(0.0f, 357.0f); Speed = 4.5f; Capacity = 0; MaxCapacity = 300; Arrived = false; Loaded = false; FactoryData = factoryData2;} |> EntityData.Truck; Behaviours = [PickupBehaviour; AIBehaviour]; Render = fun (Truck x) -> new RectangleShape(new Vector2f(40.0f, 20.0f), Position = new Vector2f(x.Position.X, x.Position.Y), FillColor=Color.White)}
+            let truck2 = {Data = {Position = new Vector2f(0.0f, 357.0f); Speed = 4.5f; Capacity = 0; MaxCapacity = 300; Arrived = false; Loaded = false; FactoryData = factoryData2;} |> EntityData.Truck; Render = fun (Truck x) -> new RectangleShape(new Vector2f(40.0f, 20.0f), Position = new Vector2f(x.Position.X, x.Position.Y), FillColor=Color.White)}
        
             let roads = (@) (addRoad 11 355.0f) (addRoad 11 105.0f)
 
-            do TransportGame.Run {Entities = (@) roads [truck; truck2; factory; factory2];} window
+            do TransportGame.Run {Entities = (@) roads [truck; truck2; factory; factory2]; Messages = []; Routines = [MainLoop]} window
 
         Run = fun (state : GameState) (window : RenderWindow) ->
             window.DispatchEvents()
@@ -39,10 +37,19 @@ let rec TransportGame : Game =
             window.Draw(new RectangleShape(new Vector2f(1280.0f, 720.0f), FillColor=Color.Green))
 
             state.Entities |> List.iter(fun e -> window.Draw(e.Render e.Data))
+            
+            let a, b, c = co_step (state.Routines.Head state state.Messages state.Entities)
 
-            let newState = {state with Entities = state.Entities 
-                                    |> List.map(fun entiteit -> {entiteit with Data = entiteit.Behaviours 
-                                                                            |> List.fold (fun s comp -> repeat comp s) entiteit.Data})}
+            //let n, m = repeat state.Entities.[0].Behaviours.[0] state.Entities.[0].Data state.Messages
+            
+            //let newEntities, newMessages = state.Entities |> List.map(fun entiteit -> {entiteit with Data = List.map (fun s comp messages -> repeat comp s messages) entiteit.Data entiteit.Behaviours state.Messages })
+
+            let newState = {state with Entities = b
+                                       Messages = a
+                           }
+
+            //let newState = {state with Entities = state.Entities 
+            //                        |> List.map(fun entiteit -> {entiteit with Data = List.fold (fun (s : EntityData) comp messages -> repeat comp s messages) entiteit.Data entiteit.Behaviours state.Messages})}
 
             window.Display()
             do TransportGame.Run newState window
